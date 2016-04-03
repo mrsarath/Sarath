9574290a42b5757a84b2b3e5e536e06627ccc818
mrsarath <mrsarath@yahoo.com>
2016-04-03 11:50:44 +0800

    New Project

---
+++ GitTestProj/src/main/java/com/sarath/springmvc/GitTestProj/repo/MemberDao.java
@@ -0,0 +1,16 @@
+package com.sarath.springmvc.GitTestProj.repo;
+
+import java.util.List;
+
+import com.sarath.springmvc.GitTestProj.domain.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
